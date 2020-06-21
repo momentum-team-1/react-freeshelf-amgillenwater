440899079c1823563c0e997c3605153dbb98c972
amgillenwater <amgillenwater@gmail.com>
2020-06-21 12:33:05 -0400

    transitioned to class

--- freeshelf/src/App.js
+++ freeshelf/src/App.js
@@ -2,25 +2,36 @@ import React from 'react'
 import './App.css'
 import books from './books.json'
 
-function App () {
-  console.log(books)
-  return (
-    <div className='App'>
-      {books.map((bookInfo, idx) => {
-        return <div key={idx}>
-          <ul>{bookInfo.title}
-            <li>By: {bookInfo.author}</li>
-            <li>{bookInfo.shortDescription}</li>
-            <li>{bookInfo.publisher}</li>
-            <li>{bookInfo.publicationDate}</li>
-            <li>{bookInfo.detailedDescription}</li>
-            {/* insert image here */}
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isToggleOn: false
+    }
+  }
 
-          </ul>
-        </div>
-      })}
-    </div>
-  )
+  // console.log(books)
+  render () {
+    return (
+      <div className='App'>
+        {books.map((bookInfo, idx) => {
+          return <div key={idx}>
+            <ul>{bookInfo.title}
+              <li>By: {bookInfo.author}</li>
+              <li>{bookInfo.shortDescription}</li>
+              <li><img src={bookInfo.coverImageURL} /></li>
+              {/* insert image here */}
+              {/* expanded view */}
+              <li>{bookInfo.publisher}</li>
+              <li>{bookInfo.publicationDate}</li>
+              <li>{bookInfo.detailedDescription}</li>
+
+            </ul>
+          </div>
+        })}
+      </div>
+    )
+  }
 }
 
 export default App
