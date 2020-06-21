3b43c4cd360a574c1b7101df996110054b91922e
amgillenwater <amgillenwater@gmail.com>
2020-06-21 11:59:43 -0400

    first go

--- freeshelf/src/App.js
+++ freeshelf/src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 import books from './books.json'
 
-function App() {
+function App () {
+  console.log(books)
   return (
-    <div className="App">
-      
+    <div className='App'>
+      {books.map((bookInfo, idx) => {
+        return <div key={idx}>
+          <ul>{bookInfo.title}
+            <li>By: {bookInfo.author}</li>
+            <li>{bookInfo.shortDescription}</li>
+            <li>{bookInfo.publisher}</li>
+            <li>{bookInfo.publicationDate}</li>
+            <li>{bookInfo.detailedDescription}</li>
+            {/* insert image here */}
+
+          </ul>
+        </div>
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
